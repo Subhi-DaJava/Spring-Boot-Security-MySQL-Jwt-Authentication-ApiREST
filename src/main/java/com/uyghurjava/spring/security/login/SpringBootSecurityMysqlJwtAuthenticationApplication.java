8275036f7836038fdd203180027f6e642fedaa5a
Subhi YD <subhy809fr@gmail.com>
2022-06-19 22:28:56 +0200

    v1.0.0 1.Setup new Spring Boot Login project

---
+++ src/main/java/com/uyghurjava/spring/security/login/SpringBootSecurityMysqlJwtAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.uyghurjava.spring.security.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityMysqlJwtAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityMysqlJwtAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uyghurjava/spring/security/login/SpringBootSecurityMysqlJwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uyghurjava.spring.security.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityMysqlJwtAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
